d41dc7ea3c64418311e7930093cc8ec9ff256b8b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-02 09:13:15 +0100

    cdhjs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container')
 const search = document.querySelector('.search-box button')
 const weatherBox = document.querySelector('.weather-box')
 const weatherDetails = document.querySelector('.weather-detail')
+const error404 = document.querySelector('.not-found')
 
 search.addEventListener('click', () => {
   const APIKey = '98740f4ebc0d63bc0f8ba70090e5a091';
@@ -11,6 +12,19 @@ search.addEventListener('click', () => {
     return;
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+    if (json.cod == '404'){
+      container.computedStyleMap.height = '400px'
+      weatherBox.classList.remove('active')
+      weatherDetails.classList.remove('active')
+      error404.classList.add('active')
+      return
+    }
+
+    container.style.height = '555px'
+    weatherBox.classList.add('active')
+    weatherDetails.classList.add('active')
+    error404.classList.remove('active')
+
     const image = document.querySelector('.weather-box img')
     const temperature = document.querySelector('.weather-box .temperature')
     const description = document.querySelector('.weather-box .description')
