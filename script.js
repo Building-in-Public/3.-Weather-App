2807d2aa2948c9e7ffdeecdadd917fb24adeb603
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-02 09:01:59 +0100

    nbnvsjdbhv

--- script.js
+++ script.js
@@ -5,4 +5,43 @@ const weatherDetails = document.querySelector('.weather-detail')
 
 search.addEventListener('click', () => {
   const APIKey = '98740f4ebc0d63bc0f8ba70090e5a091';
+  const city = document.querySelector('.search-box input').value;
+
+  if (city == '')
+    return;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+    const image = document.querySelector('.weather-box img')
+    const temperature = document.querySelector('.weather-box .temperature')
+    const description = document.querySelector('.weather-box .description')
+    const humidity = document.querySelector('.weather-details .humidity span')
+    const wind = document.querySelector('.weather-details .wind span')
+
+    switch (json.weather[0].main){
+      case 'Clear':
+        image.src = '';
+        break;
+      case 'Rain':
+        image.src = '';
+        break;
+      case 'Snow':
+        image.src = '';
+        break;
+      case 'Clouds':
+        image.src = '';
+        break;
+      case 'Mist':
+        image.src = '';
+        break;
+      case 'Haze':
+        image.src = '';
+        break;
+      default:
+        image.src = ''; 
+    }
+    temperature.innerHTML = `${parseInt(json.main.temp)}<span></span>`
+    description.innerHTML = `${json.weather[0].description}`
+    humidity.innerHTML = `${json.main.humidity}%`
+    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`
+  })
 })
