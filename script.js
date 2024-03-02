e9f94a442ec965bd0884c868a29d084874a835e2
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-02 08:43:49 +0100

    bnfx

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const container = document.querySelector('.container')
 const search = document.querySelector('.search-box button')
 const weatherBox = document.querySelector('.weather-box')
 const weatherDetails = document.querySelector('.weather-detail')
+
+search.addEventListener('click', () => {
+  const APIKey = '98740f4ebc0d63bc0f8ba70090e5a091';
+})
